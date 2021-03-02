86bd96bce0555d9d2ce2af34fd5198260f265635
Ilya Kalinin <kalinin.ilya.1922@gmail.com>
2021-03-02 19:14:14 +0300

    Initial commit

---
+++ src/main/java/CountIpsDb.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+public class CountIpsDb {
+    Map<String, Integer> countOfRepeatsIps = new HashMap<>(); // в этой Map лежит: key - сам ip,  value - количество повторений ip.
+
+    public void addCountIpsInMap(String string) {
+        if (countOfRepeatsIps.containsKey(string)) {
+            int countIp = countOfRepeatsIps.get(string);
+            countOfRepeatsIps.put(string, countIp + 1);
+        } else {
+            countOfRepeatsIps.put(string, 1);
+        }
+    }
+
+    public int getCountRepeatIp(String ip) {
+//        Integer countRepeatIp = countOfRepeatsIps.get(ip);
+        return countOfRepeatsIps.get(ip);
+    }
+
+    public boolean containIpInMapIps(String ip) { // почему-то не работает это
+        return countOfRepeatsIps.containsKey(ip);
+    }
+
+
+//    public void printMapWithCountRepeatsIps() {
+//        for (Map.Entry<String, Integer> entry : countOfRepeatsIps.entrySet()) {
+//            System.out.println(entry);
+//        }
+//    }
+//
+//    public String ipWithMaxCountVisits() {
+//        SortedSet<Integer> sortedSetAllValues = new TreeSet<>(countOfRepeatsIps.values());
+//        Integer maxCount = sortedSetAllValues.last();
+//        String villainIp = "xxx.xxx.xxx.xxx";
+//        for (Map.Entry<String, Integer> entry : countOfRepeatsIps.entrySet()) {
+//            if (entry.getValue().equals(maxCount)) {
+//                villainIp = entry.getKey();
+//            }
+//        }
+//        return villainIp;
+//    }
+}
